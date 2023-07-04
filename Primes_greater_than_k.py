bd7298e17e3b5a18e462f949b7127b13fdb7a16d
chandumukkapati <96334561+chandumukkapati@users.noreply.github.com>
2023-07-04 10:39:59 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,n//2+1):         if n%i==0:             return 0     else:         return 1 n=int(input()) l=list(map(int,input().split())) m=int(input()) c=0 for i in l:     if i>=m and prime(i):         c+=1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,18 @@
+def prime(n):
+    if n==1:
+        return 0
+    for i in range(2,n//2+1):
+        if n%i==0:
+            return 0
+    else:
+        return 1
+n=int(input())
+l=list(map(int,input().split()))
+m=int(input())
+c=0
+for i in l:
+    if i>=m and prime(i):
+        c+=1
+print(c)        
+        
+        
